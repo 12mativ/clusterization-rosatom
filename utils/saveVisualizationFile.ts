75fc7cbe7ef0540c6676450c890e8e1e5061b455
12mativ <ivan_m146@mail.ru>
2023-09-10 11:51:01 +0400

    ts8

--- utils/saveVisualizationFile.ts
+++ utils/saveVisualizationFile.ts
@@ -1,10 +1,16 @@
+//@ts-ignore
 export const saveImage = (divId, fileName) => {
+  //@ts-ignore
   const chartElement = document.getElementById(divId)
   if (chartElement) {
+    //@ts-ignore
     const svgData = chartElement.querySelector('svg')?.outerHTML
     if (svgData) {
+      //@ts-ignore
       const blob = new Blob([svgData], {type: 'image/svg+xml'})
+      //@ts-ignore
       const url = URL.createObjectURL(blob)
+      //@ts-ignore
       const a = document.createElement('a')
       a.href = url
       a.download = fileName
